38767edc22dede6ba194f8c7c30555a620d273b3
Bunny's Macbook M1 <varun.verma2024@gmail.com>
2024-10-22 18:52:11 +0530

    Setting up and Landing Page done

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+const cardImages = [
+  { "src" : "/"}
+]
+
+function App() {
+  return (
+    <>
+      <h1>HellDivers Card Memory Game</h1>
+      <button>Play</button>
+    </>
+  )
+}
+
+export default App
